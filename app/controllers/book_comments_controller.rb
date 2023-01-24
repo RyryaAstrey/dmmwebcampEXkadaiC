07844de021ad6d4a13ab9f5f372e5bf9bf420aa0
EC2 Default User <ec2-user@ip-172-31-14-254.ap-northeast-1.compute.internal>
2023-01-24 05:59:26 +0000

    [FINISH]課題６

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,6 +4,18 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to books_path
+    redirect_back(fallback_location: books_path)
   end
+  
+  def destroy
+    BookComment.find(params[:book_id]).destroy
+    redirect_back(fallback_location: books_path)
+  end
+  
+  private
+  
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+  
 end
